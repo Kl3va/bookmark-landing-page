06f6bb273c0e3a539ee96060f43deabec8e35bdd
Xurd3 <61953663+Xurd3@users.noreply.github.com>
2022-03-25 02:59:43 +0100

    added a little styling to the form section

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -8,15 +8,18 @@ const Form = ({ text, heading, placeholder, btnText, errorMessage }) => {
       <p>{text}</p>
       <h2>{heading}</h2>
       <form action="post" className="form">
-        <label htmlFor="email"></label>
-        <input
-          type="email"
-          name="email"
-          id="email"
-          placeholder={placeholder}
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
+        <div className="email-input">
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder={placeholder}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          {<p></p>}
+        </div>
+
         <input type="submit" value={btnText} />
       </form>
     </section>
