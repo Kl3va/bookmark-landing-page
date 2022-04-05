5473257e0f464d8747bccb1c5aff6e03f73e2438
Xurd3 <61953663+Xurd3@users.noreply.github.com>
2022-04-05 02:17:52 +0100

    navbar responsive

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,12 +1,16 @@
 import React from "react";
 import logo from "../logo-bookmark.svg";
 
+//Harmburger icon
+import hamburger from "../images/icon-hamburger.svg";
+
 const Navbar = ({ links }) => {
   return (
     <nav className="nav">
       <div className="nav__logo">
         <img src={logo} alt="logo" />
       </div>
+      <img src={hamburger} alt="icon-open" className="open" />
       <ul className="nav__links">
         {links.map((link) => {
           const { id, url, text } = link;
